68f2d4eb218ebcc24fccb2a128693fedbc6aff39
GoldenChicken999 <42561220+GoldenDragon1989216SangSin@users.noreply.github.com>
2018-08-30 20:13:07 -0600

    adsfsadf

--- express-server/app_hooked.js
+++
@@ -1,6 +0,0 @@
-//./express-server
-// ./express-server/app_hooked.js
-require('babel-register')({
-    presets: ['es2015-node6']
-    });
-require('./app.js');
--- react-client/src/actions/appActions.js
+++
@@ -1,7 +0,0 @@
-// ./react-redux-client/src/actions/appActions.js
-
-export const toggleAddTodo = () => {
-  return {
-    type: 'TOGGLE_ADD_TODO'
-  }
-}
